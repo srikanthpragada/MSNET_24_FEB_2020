6c854d2256fe916425efb1d943ba30db5eb4ed79
srikanthpragada <srikanthpragada@gmail.com>
2020-03-25 09:32:54 +0530

    25-MAR

---
+++ CSDemo/EF/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CsharpDemo.EF
+{
+    [Table("categories")]
+    class Category
+    {
+        [Column("catcode")]
+        [Key]
+        public string Code { get; set; }
+
+        [Column("catdesc")]
+        public string Description { get; set; }
+
+    }
+}
